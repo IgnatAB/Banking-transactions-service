281cd4a03a9d372eca40d189cecc654e6868b587
Ignat <gengash@yandex.ru>
2025-10-28 14:04:43 +0300

    optimaze poms file

--- dlq-service/src/main/java/ru/ignatab/mapper/DlqMessageMapper.java
+++ dlq-service/src/main/java/ru/ignatab/mapper/DlqMessageMapper.java
@@ -9,11 +9,10 @@ import ru.ignatab.model.DlqMessage;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface DlqMessageMapper {
-    // из DTO в сущность (добавляем причину вручную)
+
     @Mapping(target = "errorReason", source = "reason")
     DlqMessage toEntity(TransactionDto dto, String reason);
 
-    // из сущности обратно в DTO
     TransactionDto toDto(DlqMessage entity);
 
 }
